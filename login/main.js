c4b70f2b423b15a2a6f11eb403f9a15481baa8d4
StarHealix <vinicius.bastos.monteiro@gmail.com>
2022-02-04 10:10:07 -0300

    token generation and Home screen

--- login/main.js
+++ login/main.js
@@ -18,3 +18,50 @@ notSeePassword.addEventListener('click', ()=>{
         notSeePassword.setAttribute('style', 'display: none;')
         inputSenha.setAttribute('type', 'password')
 })
+
+function entrar(){
+    let usuario = document.querySelector('#usuario')
+    let userLabel = document.querySelector('#userLabel')
+
+    let password = document.querySelector('#password')
+    let senhaLabel = document.querySelector('#senhaLabel')
+
+    let msgError = document.querySelector('#msgError')
+    let listaUser = []
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+    
+    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+    
+      listaUser.forEach((item)=>{
+          if(usuario.value == item.userCad && password.value == item.senhaCad){
+             userValid = {
+                 nome: item.nomeCad,
+                 user: item.userCad,
+                 senha: item.senhaCad
+             }
+          }
+      })
+
+    if(usuario.value == userValid.user && password.value == userValid.senha){
+        window.location.href = ' http://127.0.0.1:5500/Home/index.html '
+
+     let token = Math.random().toString(16).substring(2) //na necessidade de um token maior basta concatenar 
+        localStorage.setItem('token', token)
+        localStorage.setItem('userLogado', JSON.stringify(userValid))
+
+    } else {
+        userLabel.setAttribute('style', 'color: red')
+        usuario.setAttribute('style', 'border-color: red')
+        senhaLabel.setAttribute('style', 'color: red')
+        password.setAttribute('style', 'border-color: red')
+
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = '*Usuário ou senha incorretos*'
+        
+        usuario.focus()
+    }
+}
